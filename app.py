03d7814d92cbdb39295982eee2c17c2aea6d8b34
mge112 <mge@tulane.edu>
2023-04-15 18:39:18 -0500

    1

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from datetime import datetime
 import plotly.express as px
 import plotly.graph_objects as go
 from wordcloud import WordCloud
-
+import pickle
 
 
 # your google credentials json file 
@@ -78,7 +78,7 @@ with home:
             placeholder.empty()
 
 with model:
-    st.subheader("Our Sentiment Model")
+    st.subheader("OSentiment Model")
     col1, col2, col3 = st.columns(3)
     col1.metric("Accuracy", "82%")
     col2.metric("F1 Score", "0.78")
@@ -95,13 +95,13 @@ with model:
     st.write(modified_train)
     "---"
     st.subheader("Model Training")
+    with open('patient_review_pipeline.pkl', 'rb') as f:
+        pipeline = pickle.load(f)
+    test = pd.read_csv('modified_test.csv')
+    st.dataframe(test.sample(5))
     "---"
     st.subheader("Model Testing")
     "---"
-    st.subheader("Evaluation Tuning and Performance")
-    "---"
-    st.subheader("Final Model")
-    "---"
-    st.subheader("Data Collection and Workflow")
-    "---"
+
+
     
